ba9d575a966e0dbf00b106614fe959146358ca44
JagCesar <cesar@jagcesar.se>
2022-01-29 15:32:33 +0100

    Update plugin name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'YaleHome';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge_yale_home';
+export const PLUGIN_NAME = 'homebridge_yalehome';
