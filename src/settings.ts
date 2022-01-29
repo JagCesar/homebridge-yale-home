14e425bfb7ae50f736c6b05d93eeae018dbdb388
JagCesar <cesar@jagcesar.se>
2022-01-29 14:39:50 +0100

    Use new yale dependency

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'YaleSyncAlarm';
+export const PLATFORM_NAME = 'YaleHome';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = '@henrywang/homebridge-yale-sync-alarm';
+export const PLUGIN_NAME = 'homebridge_yale_home';
